6f78c892c8ae0999ff1918aa2c2c519284ff15f3
leathur01 <147360626+leathur01@users.noreply.github.com>
2024-04-17 19:33:07 +0700

    First commit

---
+++ src/main/java/com/amess/messbook/MessbookApplication.java
@@ -0,0 +1,13 @@
+package com.amess.messbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amess/messbook/MessbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amess.messbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
