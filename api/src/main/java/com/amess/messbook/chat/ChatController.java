5d694a8bf2879d1e512e5a7985d427727e21b9ba
leathur01 <147360626+leathur01@users.noreply.github.com>
2025-04-04 17:39:30 +0700

    Refactor for readability

--- api/src/main/java/com/amess/messbook/chat/ChatController.java
+++ api/src/main/java/com/amess/messbook/chat/ChatController.java
@@ -1,63 +1,30 @@
 package com.amess.messbook.chat;
 
-import com.amess.messbook.chat.entity.Chat;
 import com.amess.messbook.chat.entity.Message;
 import com.amess.messbook.social.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final SimpMessagingTemplate simpMessagingTemplate;
-    private final MessageRepository messageRepository;
-    private final ChatRepository chatRepository;
-    private final int PAGE_SIZE = 15;
+    private final ChatService chatService;
 
     @MessageMapping("/message")
     public Message sendMessage(@Payload Message chatMessage) {
-        Optional<Chat> optionalChat = chatRepository.findBySenderNicknameAndReceiverNickname(chatMessage.getSenderNickname(), chatMessage.getReceiverNickname());
-        Chat savedChatRoom;
-        if (optionalChat.isEmpty()) {
-            Chat chatRoom = Chat.builder()
-                    .senderNickname(chatMessage.getSenderNickname())
-                    .receiverNickname(chatMessage.getReceiverNickname())
-                    .build();
-            savedChatRoom = chatRepository.save(chatRoom);
-        } else {
-            savedChatRoom = optionalChat.get();
-        }
-        chatMessage.setChatId(savedChatRoom.getId());
-        chatMessage.setCreatedAt(LocalDateTime.now());
-        Message savedChatMessage = messageRepository.save(chatMessage);
-
-        simpMessagingTemplate.convertAndSendToUser(savedChatMessage.getReceiverNickname(), "/messages", chatMessage);
-        return savedChatMessage;
+        return chatService.sendMessage(chatMessage);
     }
 
     @GetMapping("messages/{receiverNickname}")
     public Page<Message> getChatMessages(@AuthenticationPrincipal User user, @PathVariable String receiverNickname, @RequestParam(defaultValue = "5") int page) {
-        Optional<Chat> optionalChat = chatRepository.findBySenderNicknameAndReceiverNickname(user.getNickname(), receiverNickname);
-        if (optionalChat.isEmpty()) {
-            return Page.empty();
-        }
-
-        Chat chatRoom = optionalChat.get();
-        Pageable pageable = PageRequest.of(page, PAGE_SIZE);
-        return messageRepository.findByChatIdOrderByIdDesc(chatRoom.getId(), pageable);
+        return chatService.getChatMessages(user, receiverNickname, page);
     }
 }
---
+++ api/src/main/java/com/amess/messbook/chat/ChatService.java
@@ -0,0 +1,63 @@
+package com.amess.messbook.chat;
+
+import com.amess.messbook.chat.entity.Chat;
+import com.amess.messbook.chat.entity.Message;
+import com.amess.messbook.social.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class ChatService {
+
+    private final MessageRepository messageRepository;
+    private final ChatRepository chatRepository;
+    private final SimpMessagingTemplate simpMessagingTemplate;
+    private final int MESSAGES_SIZE = 15;
+
+    Message sendMessage(Message chatMessage) {
+        Message savedChatMessage = saveMessage(chatMessage);
+        simpMessagingTemplate.convertAndSendToUser(savedChatMessage.getReceiverNickname(), "/messages", chatMessage);
+        return savedChatMessage;
+    }
+
+    Message saveMessage(Message chatMessage) {
+        Chat savedChatRoom = getChatRoomAndCreateIfNotExist(chatMessage.getSenderNickname(), chatMessage.getReceiverNickname());
+        chatMessage.setChatId(savedChatRoom.getId());
+        chatMessage.setCreatedAt(LocalDateTime.now());
+        return messageRepository.save(chatMessage);
+    }
+
+    Chat getChatRoomAndCreateIfNotExist(String senderNickname, String receiverNickname) {
+        Optional<Chat> optionalChat = chatRepository.findBySenderNicknameAndReceiverNickname(senderNickname, receiverNickname);
+        ;
+        return optionalChat.orElseGet(() -> saveChatRoom(senderNickname, receiverNickname));
+    }
+
+    Chat saveChatRoom(String senderNickname, String receiverNickname) {
+        Chat chatRoom = Chat.builder()
+                .senderNickname(senderNickname)
+                .receiverNickname(receiverNickname)
+                .build();
+        return chatRepository.save(chatRoom);
+    }
+
+    Page<Message> getChatMessages(User user, String receiverNickname, int page) {
+        Optional<Chat> optionalChat = chatRepository.findBySenderNicknameAndReceiverNickname(user.getNickname(), receiverNickname);
+        if (optionalChat.isEmpty()) {
+            return Page.empty();
+        }
+
+        Chat chatRoom = optionalChat.get();
+        Pageable pageable = PageRequest.of(page, MESSAGES_SIZE);
+        return messageRepository.findByChatIdOrderByIdDesc(chatRoom.getId(), pageable);
+    }
+
+}
